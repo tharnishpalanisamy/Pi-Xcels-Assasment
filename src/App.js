9e199654f2107f5806a5be7337fde41a2bd6aa5e
Tharnish palanisamy <tharnishpalanisamy3@gmail.com>
2026-02-16 00:01:37 +0530

    Corrected files structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import ListMovies from '../ListMovies';
+import ListMovies from './ListMovies';
 import MovieDetails from './MovieDetails';
 import './App.css';
 
